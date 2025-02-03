86546e946ccc597fa9cb0edf2d742fbb93556766
abarnov687 <abarnov687@g.educaand.es>
2025-02-03 12:35:40 +0100

    Eliminada carpeta desubicada

--- ArraysBasico/RP41/rp41ejercicio10.java
+++
@@ -1,9 +0,0 @@
-public class rp41ejercicio10 {
-    public static void main(String[] args) {
-        String[] diasSemana = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo", };
-
-        for (int i = 0; i < diasSemana.length; i++) {
-            System.out.print(diasSemana[i] + " ");
-        }
-    }
-}
